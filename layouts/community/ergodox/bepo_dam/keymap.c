0e75d6a025c78449575689fd96bf60b47ece6114
Damien Merenne <dam@cosinux.org>
2017-11-29 18:07:33 +0100

    Control, Space and Enter under thumb.

--- layouts/community/ergodox/bepo_dam/keymap.c
+++ layouts/community/ergodox/bepo_dam/keymap.c
@@ -16,16 +16,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.                                  ,--------------------------------------------------.
  * |   $    |   "  |   <  |   >  |   (  |   )  |Delete|                                  |ScroLo|   @  |   +  |   -  |   /  |   *  |   =    |
  * |--------+------+------+------+------+-------------|                                  |------+------+------+------+------+------+--------|
- * |   %    |   B  |E_ACUT|   P  |   O  |E_GRAV|Backsp|                                  |CapsLo|   ^  |   V  |   D  |   L  |   J  |   Z    |
+ * |   %    |   B  |E_ACUT|   P  |   O  |E_GRAV|Backsp|                                  | Tab  |   ^  |   V  |   D  |   L  |   J  |   Z    |
  * |--------+------+------+------+------+------|  ace |                                  |      |------+------+------+------+------+--------|
  * |   W    |   A  |   U  |   I  |   E  |   ,  |------|                                  |------|   C  |   T  |   S  |   R  |   N  |   M    |
- * |--------+------+------+------+------+------|  Tab |                                  | NumLo|------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|      |                                  |      |------+------+------+------+------+--------|
  * | E_CIRC |A_GRAV|   Y  |   X  |   .  |   K  |      |                                  |      |   '  |   Q  |   G  |   H  |   F  | C_CEDIL|
  * `--------+------+------+------+------+-------------,-------------.      ,-------------`-------------+------+------+------+------+--------'
- *   | Lalt |Insert|LSuper| LCtrl|Escape|             | BEPO |QWERTY|      |AZERTY| BEPO |             | AltGr| RCtrl|RSuper|PrntSc| Ralt|
+ *   | Lalt |Insert|LSuper|      |Escape|             |      |      |      |      |      |             | AltGr|      |RSuper|PrntSc| Ralt |
  *   `----------------------------------'      ,------|------|------|      |------+------+------.      `----------------------------------'
- *                                             |      |      |L_NumK|      |L_NumK|      |      |
- *                                             | Space|LShift|------|      |------|RShift|Enter |
+ *                                             | space|      |L_NumK|      |L_NumK|      |enter |
+ *                                             | LCtrl|LShift|------|      |------|RShift|RCtrl |
  *                                             |      |      |L_FNav|      |L_FNav|      |      |
  *                                             `--------------------'      `--------------------'
  */
@@ -34,20 +34,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 BP_DOLLAR,	BP_DQOT,	BP_LGIL,	BP_RGIL,	BP_LPRN,	BP_RPRN,	KC_DEL,
 BP_PERCENT,	BP_B,		BP_E_ACUTE,	BP_P,		BP_O,		BP_E_GRAVE,	KC_BSPC,
 BP_W,		BP_A,		BP_U,		BP_I,		BP_E,		BP_COMMA,
-BP_ECRC,	BP_A_GRAVE,	BP_Y,		BP_X,		BP_DOT,		BP_K,		KC_TAB,
-KC_LALT,	KC_INS,		KC_LGUI,	KC_LCTL,	KC_LALT,
-														DF(BEPO),	DF(QW_B),
+BP_ECRC,	BP_A_GRAVE,	BP_Y,		BP_X,		BP_DOT,		BP_K,		KC_NO,
+KC_LALT,	KC_INS,		KC_LGUI,	KC_NO,		KC_LALT,
+														KC_NO,		KC_NO,
 																MO(NUMK),
-												KC_SPC,		KC_LSHIFT,	MO(FNAV),
+										MT(MOD_LCTL, KC_SPACE),		KC_LSHIFT,	MO(FNAV),
 // Right hand
 				KC_SLCK,	BP_AT,		BP_PLUS,	BP_MINUS,	BP_SLASH,	BP_ASTR,	BP_EQUAL,
-				KC_CAPSLOCK,	BP_DCRC,	BP_V,		BP_D,		BP_L,		BP_J,		BP_Z,
+				KC_TAB,		BP_DCRC,	BP_V,		BP_D,		BP_L,		BP_J,		BP_Z,
 						BP_C,		BP_T,		BP_S,		BP_R,		BP_N,		BP_M,
-				KC_NUMLOCK,	BP_APOS,	BP_Q,		BP_G,		BP_H,		BP_F,		BP_CCED,
+				KC_NO,		BP_APOS,	BP_Q,		BP_G,		BP_H,		BP_F,		BP_CCED,
 								BP_ALGR,	KC_RCTL,	KC_RGUI,	KC_PSCREEN,	KC_RALT,
-DF(AZ_B),	DF(BEPO),
+KC_NO,		KC_NO,
 MO(NUMK),
-MO(FNAV),	KC_RSHIFT,	KC_ENTER),
+MO(FNAV),	KC_RSHIFT,	MT(MOD_RCTL, KC_ENTER)),
 /* Keymap 7: function / navigation / mouse layer
  *
  * ,--------------------------------------------------.                                  ,--------------------------------------------------.
@@ -56,7 +56,7 @@ MO(FNAV),	KC_RSHIFT,	KC_ENTER),
  * |        | Next |LClick|  Up  |RClick| WhUp |VolDwn|                                  |      | PgUp | Home |  Up  |  End |  F11 |        |
  * |--------+------+------+------+------+------|      |                                  |      |------+------+------+------+------+--------|
  * |        | Prev | Left | Down | Right|WhDown|------|                                  |------| PgDn | Left | Down | Right|  F12 |        |
- * |--------+------+------+------+------+------| VolUp|                                  |      |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| VolUp|                 =                |      |------+------+------+------+------+--------|
  * |        | Undo |  Cut | Copy | Paste|      |      |                                  |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------,-------------.      ,-------------`-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |             |      |      |      |      |      |             |      |      |      |      |      |
